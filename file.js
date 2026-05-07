db2b78b445ef64ae62e3792fbf80be3c69070f97
deberlycruz <debbiecruz013@gmail.com>
2026-05-07 13:42:55 +0800

    Add files via upload

---
+++ file.js
@@ -0,0 +1,19 @@
+// Highlight navbar item on scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 80;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
